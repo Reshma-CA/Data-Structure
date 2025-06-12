41a6bc570f90bd4998efcf6cfacb6c615adac9da
Reshma.CA <reshmacaci@gmail.com>
2025-06-13 00:04:49 +0530

    binary search change

--- Week 1/Serching_Algorithms/binary_search.py
+++ Week 1/Serching_Algorithms/binary_search.py
@@ -1,25 +1,25 @@
 Arr = [10,20,30,40,50,60]
 
-def binary_search(arr):
-    target = 30
-    left = 0
-    right = len(arr)-1
-    
 
-    while(left<=right):
-        mid = left+right//2
-        if Arr[mid] == target:
-            print(f"Found target at this index position {mid} and value {Arr[mid]}")
-            break
+target = 30
+left = 0
+right = len(Arr)-1
 
-        elif arr[mid]>target:
-            right = mid-1
 
-        else:
-            left = mid+1
+while(left<=right):
+    mid = left+right//2
+    if Arr[mid] == target:
+        print(f"Found target at this index position {mid} and value {Arr[mid]}")
+        break
 
-    if(left>right):
-        print("Target element not found in this array")
+    elif Arr[mid]>target:
+        right = mid-1
+
+    else:
+        left = mid+1
+
+if(left>right):
+    print("Target element not found in this array")
 
 #############################################################################
 # using function
